65df9fb1df9f95b3cbd3a4c4b6f538f81bb8ec2a
Gabriel Martin <g.martin@htlkrems.at>
2023-12-03 10:40:29 +0100

    2023 Day 2 Part 1

---
+++ 2023/Day2-Part1/aoc.py
@@ -0,0 +1,39 @@
+
+
+def main():
+    file = open("input", "r")
+    result = 0
+    while True:
+        line = file.readline()
+        if not line:
+            break
+        data = line.split(":")
+        game_number = int(data[0].split(" ")[1])
+        draws = data[1].split(";")
+        success = True
+        for draw in draws:
+            if not success:
+                break
+            cubes = draw.split(",")
+            for cube in cubes:
+                cube_data = cube.split(" ")
+                amount = int(cube_data[1])
+                cube_color = cube_data[2]
+                if cube_color == "red" and amount > 12:
+                    success = False
+                    break
+                if cube_color == "green" and amount > 13:
+                    success = False
+                    break
+                if cube_color == "blue" and amount > 14:
+                    success = False
+                    break
+        if success:
+            result += game_number
+
+    print(result)
+
+if __name__ == "__main__":
+    main()
+
+
